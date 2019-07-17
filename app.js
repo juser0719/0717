e50a87a26a14a15e101fe7c56ca4d92b6ef8ad03
kibnm821 <kibnm821@naver.com>
2019-07-17 11:35:02 +0900

    Park

--- app.js
+++ app.js
@@ -13,8 +13,21 @@ router.get('/Typography', function (req, res) {
 res.sendFile(path.join(__dirname + '/views/Park/Typography.html'));
 //__dirname : It will resolve to your project folder.
 });
+
 router.get('/Typography2', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/Park/Typography2.html'));
+});
+
+router.get('/Table', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/Park/Table.html'));
+});
+
+router.get('/Tab;e2', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/Park/Table2.html'));
+});
+
+router.get('/image', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/Park/image.html'));
 });
 
 app.use('/', router);
