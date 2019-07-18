74c7b646a4d9f98f49ce86379f543e2a48e7ce12
juser0719 <sinsa0719@naver.com>
2019-07-18 17:33:35 +0900

    jeon

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const app = express()
 const port = 3000
 
 //app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
 const path = require('path');
 const router = express.Router();
 
@@ -16,24 +13,40 @@ res.sendFile(path.join(__dirname + '/views/Park/Typography.html'));
 //__dirname : It will resolve to your project folder.
 });
 
-router.get('/Typography2', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/Park/Typography2.html'));
+router.get('/boot_j', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/jeon/boot_j.html'));
 });
 
-router.get('/Table', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/Park/Table.html'));
+router.get('/main_j', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/jeon/main_j.html'));
 });
 
-router.get('/Tab;e2', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/Park/Table2.html'));
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/jeon/test.html'));
 });
 
-router.get('/image', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/Park/image.html'));
+router.get('/detail', function (req, res) {
+    console.log(req.query);
+    res.send('Request parameters : '+req.query.email)
+     });
+
+router.get('/info', function (req, res) {
+    console.log(req.query);
+    res.send('Request Email :  '+req.query.email+'<br> Requset Password : '+req.query.password)
+
 });
 
-router.get('/Buttons', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/Park/Buttons.html'));
+router.get('/info2', function (req, res) {
+    console.log(req.query);
+    res.send('Request selected val :  '+req.query.list+'<br> Requset selected val : '+req.query.list2)
+
+});
+
+router.get('/info3', function (req, res) {
+    console.log(req.query);
+    res.send('Request ID :  '+req.query.id+'<br> Requset Password : '+req.query.pw+'<br> Request text : ' +req.query.text)
+
 });
 
 app.use('/', router);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
